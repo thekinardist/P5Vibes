f06cba3375ede8574b1100f2ce201763b64f3106
Justin B. Kinard <thekinardist@gmail.com>
2025-09-30 09:23:48 -0400

    Gaussian Distribution Changes
    - Updated index.html to include library for recording canvas[es]

--- GaussianDistribution/sketch.js
+++ GaussianDistribution/sketch.js
@@ -1,13 +1,25 @@
 function setup() {
-  createCanvas(600, 400);
-  background(220);
+  createCanvas(600, 240);
+  background(0);
   head = loadImage("Me_Head.png");
 }
 
 function draw() {
-  let x = 60 * randomGaussian() + 300;
-  let y = 30 * randomGaussian() + 200
-  noStroke();
-  tint(255,40);
-  image(head, x, 120, 60,60);
+  if (keyIsDown(UP_ARROW) === true){
+    let x = 65 * randomGaussian() + 300;
+    let y = 30 * randomGaussian() + 200
+    noStroke();
+    tint(255,40);
+    image(head, x, 80, 60,60);
+  }
+
+}
+
+
+//clear the canvas
+function keyPressed(){
+  if(key === 'c'){
+    clear();
+    background(0);
+  }
 }
